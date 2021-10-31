11d8b794f03d044a6320872e216cb9f8dc91e793
Cédric Walter <cedric.walter@jarowa.ch>
2021-10-31 08:22:27 +0100

    add code to read CHF-S on-chain balance

--- negative-interest-rate/README.md
+++ negative-interest-rate/README.md
@@ -1,2 +1,9 @@
-A sample command-line application with an entrypoint in `bin/`, library code
-in `lib/`, and example unit test in `test/`.
+# Implementing negative interest rate
+Negative interest rates are a form of monetary policy that sees interest rates fall below 0%.
+Central banks and regulators use this unusual policy tool when there are strong signs of deflation.
+
+We model the negative interest rate of -0.75% by leveraging the **clawback transaction** Algorand functionality.
+
+negative-interest-rate/lib/apply-negative-interest-rate.dart  run and iterate on all CHF-S account 
+holder to remove yearly 7.5% of their balance
+
--- negative-interest-rate/lib/apply-negative-interest-rate.dart
+++ negative-interest-rate/lib/apply-negative-interest-rate.dart
@@ -1,15 +1,38 @@
 import 'package:algorand_dart/algorand_dart.dart';
 
+final algodClient = AlgodClient(
+  apiUrl: 'http://54.93.225.56:4001/',
+  apiKey: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+  // apiUrl: Platform.isAndroid
+  //     ? 'http://10.0.2.2:4001'
+  //     : 'http://169.254.108.248:4001',
+  // apiKey: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+);
+
+algorand = Algorand(algodClient: algodClient);
+final ASSET_ID = 41960755; // identifying index of the asset
+
+Future<double> getAssetAmount({
+  String publicAddress,
+}) async {
+  final information = await algorand.getAccountByAddress(publicAddress);
+  for (var asset in information.assets) {
+    if (asset.assetId == ASSET_ID) {
+      return asset.amount / 100;
+    }
+  }
+}
+
 // This code is meant to be run per account after one year in Julius Bar backend system
 void main() async {
   // CHF-S private key, should not be here ;-)
   final juliusBarManagerMnemonic =
       'beauty nurse season autumn curve slice cry strategy frozen spy panic hobby strong goose employ review love fee pride enlist friend enroll clip ability runway';
 
-  final account1 = await Account.fromSeedPhrase(
+  final juliusBarManagerAccount = await Account.fromSeedPhrase(
       juliusBarManagerMnemonic.split(' '));
 
-  print('Julius Bar public address 1: ${account1.publicAddress}');
+  print('Julius Bar public address 1: ${juliusBarManagerAccount.publicAddress}');
 
   final clawbackPrivateKey = to_private_key(mnemonic);
   final clawbackAddress = address_from_private_key(clawbackPrivateKey);
@@ -18,10 +41,9 @@ void main() async {
   final firstValidRound = 1000;
   final lastValidRound = 2000;
   final genesisHash = 'SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI=';
-  final index = 41960755; // identifying index of the asset
 
-  // where to send the revoked assets, should be Swiss National Bank account!
-  final swissNationlBankAccountReceiver = generate_account();
+  // where to send the revoked CHF-S assets, should be Swiss National Bank account!
+  final swissNationalBankAccountReceiver = generate_account();
 
   // here we would use Alice, Bob or Kevin public account address in a loop
   final alicePublicAddress =  'QZRPIHE6TM7D7Z7W5SCPHJLVBGC2T6YA3HP3VUYWTL3P326C4BFXLLNXBI';
@@ -31,26 +53,35 @@ void main() async {
   // address to revoke assets from.
   final targetAddress = alicePublicAddress;
 
-  // read balance in CHF-S
-  final balance = 200; // TODO
-
   final negativ_interest_rate = 7.5 / 100;
 
-  final penalty = balance - balance * negativ_interest_rate;
+  // read on-chain balance of CHF-S
+  final chfSbalance = await getAssetAmount(targetAddress)
+  final chfSPenalty = balance - chfSbalance * negativ_interest_rate;
 
+  // here we would get fiat CHF balance of account using bank back office system
+  final chfBalance = await getFiatBalance(targetAddress)
+  final chfPenalty = balance - chfBalance * negativ_interest_rate;
+  
   // create the asset config transaction
-  final txn = AssetTransferTxn(
+  final tx = AssetTransferTxn(
     sender: clawbackAddress,
     fee: feePerByte,
     first_valid_round: firstValidRound,
     last_valid_round: lastValidRound,
     genesis_hash: genesisHash,
-    index: index,
+    index: ASSET_ID,
     receiver: receiver.address,
-    amt: penalty,
+    amt: chfSPenalty,
     revocation_target: targetAddress,
   );
 
-  // sign the transction
-  final signedTxn = txn.sign(clawbackPrivateKey);
+  // Sign the transaction
+  final signedTx = await tx.sign(juliusBarManagerAccount);
+
+  // Broadcast the transaction
+  final txId = await algorand.sendTransaction(signedTx);
+  final response = await algorand.waitForConfirmation(txId);
+  print(response);
+  print(response);
 }
